dbcec9fa2dcb924d8217994b75399fc990b4ae76
Timur Kalimullin <timur_kalimullin@list.ru>
2022-05-23 01:01:10 +0500

    add typeorm, config and connection

---
+++ ormconfig.ts
@@ -0,0 +1,11 @@
+import { DataSourceOptions } from 'typeorm';
+
+export const config: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: Number(process.env.POSTGRES_PORT) ?? 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
+import { config } from '../ormconfig';
 
 @Module({
-  imports: [TagModule],
+  imports: [TypeOrmModule.forRoot(config), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
